0eb98a971639c4bc1bf54f65a3de78237008491c
Nhuly0812 <nguyennhuly08122004@gmail.com>
2024-03-12 23:41:22 +0700

    nn

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     express.urlencoded({
         extended: true,
-    }),
+             }),
 );
              app.use(express.json()
              );
